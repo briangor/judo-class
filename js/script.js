c82be8831d18a4080d3792b6b0dc6f6a4ab83a91
Brian <briangor13@gmail.com>
2021-03-26 00:00:09 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var app = angular.module("khayrJudoApp", []);
 
 app.controller("khayrCtrl",function($scope){
-  $scope.squad = ["Zeus", "Roman", "Milan", "Rogue", "Fl",];
+  $scope.squad = ["Zeus", "Roman", "Milan", "Rogue", "Gozzo",];
   $scope.belt = ["white", "orange", "brown", "blue", "purple", "grey", "black"];
   
  
