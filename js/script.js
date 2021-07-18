cebbb28328753a9377ce052dfe07e2f8c0e15beb
Brian <briangor13@gmail.com>
2021-07-19 01:39:56 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,5 +4,15 @@ app.controller("khayrCtrl",function($scope){
   $scope.squad = ["Zeus", "Roman", "Milan", "Rogue", "Gozzo",];
   $scope.belt = ["white", "orange", "brown", "blue", "purple", "grey", "black"];
   
- 
+  $scope.addMember = function () {
+      if ($scope.squad.indexOf($scope.addMemberX) == -1){
+        $scope.squad.push($scope.addMemberX);
+      }else{
+        $scope.errortext = "The member exists";
+      }
+  }
+  $scope.removeMember = function(x) {
+    $scope.errortext = "";
+    $scope.squad.splice(x, 1);
+  }
 });
